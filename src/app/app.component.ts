7f232752461b12fd676485d56f62ae5b6b998c52
unknown <adelina.tataru@sens.media>
2021-10-06 21:06:57 +0300

    Laboratorul 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import {UserInterface} from "./user.interface";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Laboratoare-Proiect';
+export class AppComponent implements OnInit {
+  title = 'List of users';
+  users: UserInterface[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<UserInterface[]>('https://jsonplaceholder.typicode.com/users').subscribe(res => {
+      this.users = res;
+    })
+
+
+  }
 }
