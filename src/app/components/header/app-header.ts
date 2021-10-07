c91be954acc462c059355a37a826f614b9d02b82
unknown <adelina.tataru@sens.media>
2021-10-07 10:46:34 +0300

    added header and styled it

--- src/app/components/header/app-header.ts
+++ src/app/components/header/app-header.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.html',
+  styleUrls: ['./app-header.css']
 })
 
 export class AppHeader {
