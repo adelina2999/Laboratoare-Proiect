cb6577ced3754ee9a104274aa32df6e5bc7f6130
unknown <adelina.tataru@sens.media>
2021-10-07 12:24:01 +0300

    added some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import  { AppRoutingModule } from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { AppHeader } from "./components/header/app-header";
 import { ContactsComponent } from "./components/contacts/contacts.component";
 import { MenuComponent } from "./components/menu/menu.component";
 import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from "./components/home/app-home";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HomeComponent } from "./components/home/app-home";
     BrowserModule,
     HttpClientModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   exports: [RouterModule],
   providers: [],
