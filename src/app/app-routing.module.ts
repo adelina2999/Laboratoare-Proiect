d7f35fe3fac3488bf0cb96536eb99b384b1a8405
unknown <adelina.tataru@sens.media>
2021-10-07 10:58:18 +0300

    created route for homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MenuComponent} from "./components/menu/menu.component";
 import {ContactsComponent} from "./components/contacts/contacts.component";
+import { HomeComponent } from "./components/home/app-home";
 
 const routes: Routes = [
   { path: 'menu', component: MenuComponent },
   { path: 'contacts', component: ContactsComponent },
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppHeader } from "./components/header/app-header";
 import { ContactsComponent } from "./components/contacts/contacts.component";
 import { MenuComponent } from "./components/menu/menu.component";
 import { HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from "./components/home/app-home";
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
     MenuComponent,
-    AppHeader
+    AppHeader,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
